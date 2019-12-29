258728e886205fb5b2222b803d448c226fda774a
paramore2k <paramore2k@live.ca>
2019-12-29 14:23:41 -0500

    Exercise 14 complet

--- Programmation1/chapitre3/js/exercise14.js
+++ Programmation1/chapitre3/js/exercise14.js
@@ -1,3 +1,15 @@
 /*
 Afficher les nombres impairs de 1 à 49, et mettre 5 valeurs par ligne.
  */
+
+var nbImpairs = 1;
+var car = 0;
+for (i=nbImpairs;i<=49;i+=2) {
+    document.write(i + " ");
+    car++;
+    if (car === 5){
+
+        document.write('<br/>');
+    car = 0;
+    }
+}
