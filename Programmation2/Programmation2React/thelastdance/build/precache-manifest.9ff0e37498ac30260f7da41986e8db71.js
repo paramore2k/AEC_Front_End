b024cb83c4b565cd526eec1ed1dc4170e8421408
paramore2k <paramore2k@live.ca>
2020-04-21 20:11:14 -0400

    Added build

---
+++ Programmation2/Programmation2React/thelastdance/build/precache-manifest.9ff0e37498ac30260f7da41986e8db71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249ade6a0c1694e00a708c900f0ebf64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d2f047af1400e7ceb6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21837a082101ab63b78e",
+    "url": "/static/js/2.5730b4a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5730b4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d2f047af1400e7ceb6",
+    "url": "/static/js/main.98750f92.chunk.js"
+  },
+  {
+    "revision": "2c1d1ed8681956116811",
+    "url": "/static/js/runtime-main.d26b30a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
