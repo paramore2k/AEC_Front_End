f8b800e350b411f5462eb9927a6f5f761946b61a
paramore2k <paramore2k@live.ca>
2020-04-21 20:40:47 -0400

    Merge branch 'master' of https://github.com/paramore2k/AEC_Front_End
    # Conflicts:
    #	Programmation2/Programmation2React/thelastdance/.gitignore

---
+++ Programmation2/thelastdance/build/precache-manifest.0d1ddbdc549e5289158f683654b799ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7112af58c02d55b9d7fac1807353dd9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d134ec7f7868c5115b1a",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "21837a082101ab63b78e",
+    "url": "./static/js/2.5730b4a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5730b4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d134ec7f7868c5115b1a",
+    "url": "./static/js/main.70aa3659.chunk.js"
+  },
+  {
+    "revision": "a516097f580751f773f1",
+    "url": "./static/js/runtime-main.7944e585.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
