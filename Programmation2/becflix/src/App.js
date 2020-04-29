ab33b4b1be351b9cb383ffde09ca59d47e770a10
paramore2k <paramore2k@live.ca>
2020-04-29 12:14:04 -0400

    Application The Last Dance fonctionnel avec npm run build

---
+++ Programmation2/becflix/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Programmation2/thelastdance/build/precache-manifest.831fc617ea1fd05b8ac837a2a34af001.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d65ce76c6f6dd16cfcc6e284894cf4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64a2bbe9dc21eb7fda89",
+    "url": "./static/css/main.ac55cac6.chunk.css"
+  },
+  {
+    "revision": "40874c2c5a44af357bbc",
+    "url": "./static/js/2.8ee066b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8ee066b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a2bbe9dc21eb7fda89",
+    "url": "./static/js/main.dea86b49.chunk.js"
+  },
+  {
+    "revision": "66474e4d123a12ff0a8e",
+    "url": "./static/js/runtime-main.925ff67e.js"
+  },
+  {
+    "revision": "d93679312b945167b1aca7932e1ba596",
+    "url": "./static/media/billwennington.d9367931.png"
+  },
+  {
+    "revision": "9e94bc224eb00474b8efbede1c5e959d",
+    "url": "./static/media/dennisrodman.9e94bc22.jpg"
+  },
+  {
+    "revision": "161c7935d9ac5578fd5c626c801d9a5d",
+    "url": "./static/media/michaeljordan.161c7935.jpg"
+  },
+  {
+    "revision": "13c04be481d48be26878c9d528ea7369",
+    "url": "./static/media/scottiepipen.13c04be4.jpg"
+  },
+  {
+    "revision": "dd4cdbbdc8d7455e907a587443724401",
+    "url": "./static/media/stevekeer.dd4cdbbd.jpg"
+  }
+]);
