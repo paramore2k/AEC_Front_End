febf26218a0ce75bce6e6ee74cb2f18190c44fd2
paramore2k <paramore2k@live.ca>
2020-06-23 18:35:24 -0400

    Musify 100% fonctionnel avec ajout du bouton PLAY sur hover

--- Programmation2/musify/build/service-worker.js
+++ Programmation2/musify/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2edf3661ef99a2fc977fcd21bc19f884.js"
+  "./precache-manifest.418ef51e208e39730f2cda7b10ab8142.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
