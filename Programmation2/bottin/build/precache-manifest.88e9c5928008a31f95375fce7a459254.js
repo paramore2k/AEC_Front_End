9cc0280a138743aefeb549b43db6aef22bdec5ef
paramore2k <paramore2k@live.ca>
2020-06-24 10:55:43 -0400

    bottin rebuild

---
+++ Programmation2/bottin/build/precache-manifest.88e9c5928008a31f95375fce7a459254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf1d4aff06487eb24ce6eada138bccd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbafb5b6250e57778989",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e5dfe9da2e480edd3d48",
+    "url": "./static/css/main.74d298ad.chunk.css"
+  },
+  {
+    "revision": "cbafb5b6250e57778989",
+    "url": "./static/js/2.c4c8252c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.c4c8252c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5dfe9da2e480edd3d48",
+    "url": "./static/js/main.91cec51e.chunk.js"
+  },
+  {
+    "revision": "4573c68933a7872b3065",
+    "url": "./static/js/runtime-main.bcc04e5e.js"
+  }
+]);
