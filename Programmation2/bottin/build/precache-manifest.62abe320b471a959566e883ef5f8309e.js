621207312d5a567605fa7234d3e04334e574ca10
paramore2k <paramore2k@live.ca>
2020-06-24 11:02:25 -0400

    bottin rebuild

---
+++ Programmation2/bottin/build/precache-manifest.62abe320b471a959566e883ef5f8309e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71054aae9fd3f86b9a7430a719201c67",
+    "url": "/AEC_Front_End/Programmation2/bottin/index.html"
+  },
+  {
+    "revision": "cbafb5b6250e57778989",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dea4b2d1934fbd346243",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/css/main.74d298ad.chunk.css"
+  },
+  {
+    "revision": "cbafb5b6250e57778989",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/js/2.c4c8252c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/js/2.c4c8252c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea4b2d1934fbd346243",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/js/main.4474834a.chunk.js"
+  },
+  {
+    "revision": "5a73b90a6741ccb52162",
+    "url": "/AEC_Front_End/Programmation2/bottin/static/js/runtime-main.89685e26.js"
+  }
+]);
--- Programmation2/bottin/build/service-worker.js
+++ Programmation2/bottin/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.88e9c5928008a31f95375fce7a459254.js"
+  "/AEC_Front_End/Programmation2/bottin/precache-manifest.62abe320b471a959566e883ef5f8309e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AEC_Front_End/Programmation2/bottin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
