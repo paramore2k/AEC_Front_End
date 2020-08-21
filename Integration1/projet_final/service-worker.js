731bc31ef1ffb2a0e82241c2defd7a1aa620ec24
paramore2k <paramore2k@live.ca>
2020-08-20 20:09:31 -0400

    Service Worker

--- Integration1/projet_final/service-worker.js
+++ Integration1/projet_final/service-worker.js
@@ -33,3 +33,41 @@ self.addEventListener('install', (evt) => {
     );
     self.skipWaiting();
 });
+
+//Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v2';
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+    //Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache',
+                        key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('/AEC_Front_End/Integration1/projet_final/offline.html' );
+                    });
+            })
+    );
+});
