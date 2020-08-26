0d8b75ae7ed189758d6b6dacfafa4565ff15d648
paramore2k@live.ca <Ping0uin>
2020-08-25 22:59:10 -0400

    PWA Prog et React

--- Programmation2/localprojectpwa/src/App.js
+++ Programmation2/localprojectpwa/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './img/pwa.png';
-import './App.css';
-import BoutonInstallPWA from "./components/BoutonInstallPWA";
+import BoutonInstallPWA from './components/BoutonInstallPWA';
 import DemoJSONLocal from './components/DemoJSONLocal';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <BoutonInstallPWA/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="300 px" height="100 px" />
-        <p>
-          Hello PWA World !
-        </p>
-
-      </header>
-        <DemoJSONLocal/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BoutonInstallPWA/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" width="400 px" height="100 px"/>
+                <p>
+                    Hello PWA World!
+                </p>
+            </header>
+            <DemoJSONLocal/>
+        </div>
+    );
 }
 
 export default App;
