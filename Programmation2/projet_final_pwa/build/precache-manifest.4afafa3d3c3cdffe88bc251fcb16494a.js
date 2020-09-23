9d5fadfd0284b5214271a598b7b678ab3e879ddd
paramore2k@live.ca <Ping0uin>
2020-09-23 13:28:15 -0400

    Modification de package pour le homepage dir

---
+++ Programmation2/projet_final_pwa/build/precache-manifest.4afafa3d3c3cdffe88bc251fcb16494a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adab35ecf0e680963d08f05dcd71ab4e",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/index.html"
+  },
+  {
+    "revision": "fb0be875b613d9b2f126",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/css/2.df0e23d8.chunk.css"
+  },
+  {
+    "revision": "0c7ed5d1626d572c6e70",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/css/main.eaecce4a.chunk.css"
+  },
+  {
+    "revision": "fb0be875b613d9b2f126",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/js/2.5c8a4eb9.chunk.js"
+  },
+  {
+    "revision": "02319550b42b2562b0f0cbc36f2d31db",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/js/2.5c8a4eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7ed5d1626d572c6e70",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/js/main.b9ea245d.chunk.js"
+  },
+  {
+    "revision": "d345fbad9920d37aa93e",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/js/runtime-main.4df99e85.js"
+  },
+  {
+    "revision": "284faf97384fa6802c1188b8234fb1ab",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/abominable.284faf97.jpg"
+  },
+  {
+    "revision": "20fdda8f575328f84543fa201be4ca05",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/ava_br.20fdda8f.jpg"
+  },
+  {
+    "revision": "29ae1727a3de6ddc2157bbc6933de404",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/banner_error_404.29ae1727.jpg"
+  },
+  {
+    "revision": "415a21d8c5172be8805e17fe3fe40a79",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/beetlejuice.415a21d8.jpg"
+  },
+  {
+    "revision": "8799114a83b1a1d74a1fd1529a1a4847",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/dbs3.8799114a.jpg"
+  },
+  {
+    "revision": "7e58a68aab328b786477bd9da004c798",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/el_camino.7e58a68a.jpg"
+  },
+  {
+    "revision": "81bc23a2bc735de5966a1c024632d80b",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fast_furious.81bc23a2.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e4ed3fbd8098fd31638bbfe0551be8c9",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/gb12.e4ed3fbd.jpg"
+  },
+  {
+    "revision": "8d185985441b498f023062e0ea2ca646",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/halloween.8d185985.jpg"
+  },
+  {
+    "revision": "9bd390fbacfca6968f37ca274911b3f2",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/irresistible.9bd390fb.jpg"
+  },
+  {
+    "revision": "ec52ebe933fe179ce7f0b191dd270737",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/jaws_movie_collection.ec52ebe9.jpg"
+  },
+  {
+    "revision": "e4b2ee68a15372014fae44f001595adf",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/movie_collection1.e4b2ee68.jpg"
+  },
+  {
+    "revision": "1e58bf53c60929b70331e3ddc9d0cf7b",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/outpost.1e58bf53.jpg"
+  },
+  {
+    "revision": "c628fdd6a90becf23e668f57f36f9597",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/primal.c628fdd6.jpg"
+  },
+  {
+    "revision": "221ddf0df1e3e047d625db6b5bc08b2a",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/shutter_island_br.221ddf0d.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f67fb9e27d54c5644d07f2717a3571f4",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/staten_island.f67fb9e2.jpg"
+  },
+  {
+    "revision": "fdf0a00143f9ac7bb972ab6d286774a7",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/stephen_king_br.fdf0a001.jpg"
+  },
+  {
+    "revision": "4a6ee1841d5ef180802ca41e9491d93a",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/two_jakes.4a6ee184.jpg"
+  },
+  {
+    "revision": "e6f90d0e7fecd70042c9456ebfeda448",
+    "url": "/AEC_Front_End/Programmation2/projetfinalreact/build/static/media/whiplash.e6f90d0e.jpg"
+  }
+]);
